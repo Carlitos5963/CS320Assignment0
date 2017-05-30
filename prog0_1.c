90f506645b0dbe4f5a9b17f5e7c5e3ef705140c7
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-05-29 21:43:53 -0700

    initial commit

---
+++ prog0_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+char name[256];
+char *cp;
+cp = name;
+int i;
+int j;
+
+printf("Assignment #0-1, Jose Carlos Gomez, JoseCarlosGomez69@gmail.com\n");
+
+printf("What is your name?\n");
+
+for(i = 0; i < 256; i++){
+scanf("[^\n]%c", (&name[i]));
+if(name[i] == '\n')
+break;
+}
+
+int len = strlen(name);
+printf("Hello ");
+for(j = 0; j < len; j++){
+printf("%c", name[j]);
+}
+printf("!");
+
+}
