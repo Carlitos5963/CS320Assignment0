5a8b5dea05d87faa1a927c7f25fdd04cccebc6b1
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-05-30 06:38:00 +0000

    Update prog0_1.c

--- prog0_1.c
+++ prog0_1.c
@@ -13,7 +13,7 @@ printf("Assignment #0-1, Jose Carlos Gomez, JoseCarlosGomez69@gmail.com\n");
 printf("What is your name?\n");
 
 for(i = 0; i < 256; i++){
-scanf("[^\n]%c", (&name[i]));
+scanf("%c", &name[i]);
 if(name[i] == '\n')
 break;
 }
@@ -21,7 +21,7 @@ break;
 int len = strlen(name);
 
 
-printf("\nHello ");
+printf("Hello ");
 for(j = 0; j < len-1; j++){
 printf("%c", name[j]);
 }
