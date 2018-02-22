fc3ce4c066a7a51081d818f5dd9b06703f6b024e
Carlitos5963 <carlos.gomez5963@gmail.com>
2018-02-21 16:54:06 -0800

    Update prog0_1.c

--- prog0_1.c
+++ prog0_1.c
@@ -2,29 +2,32 @@
 #include <string.h>
 
 int main(){
-char name[256];
-char *cp;
-cp = name;
-int i;
-int j;
+  char name[256];
+  char *cp;
+  cp = name;
+  int i,j;
 
-printf("Assignment #0-1, Jose Carlos Gomez, JoseCarlosGomez69@gmail.com\n");
+  //Assignment tagline
+  printf("Assignment #0-1, Jose Carlos Gomez, JoseCarlosGomez69@gmail.com\n");
 
-printf("What is your name?\n");
+  printf("What is your name?\n");
 
-for(i = 0; i < 256; i++){
-scanf("%c", &name[i]);
-if(name[i] == '\n')
-break;
-}
+  //Separate each char from user input and place in array
+  for(i = 0; i < 256; i++){
+    scanf("%c", &name[i]);
+    if(name[i] == '\n')
+    break;
+  }
 
-int len = strlen(name);
+  //Length of input
+   int len = strlen(name);
 
 
-printf("Hello ");
-for(j = 0; j < len-1; j++){
-printf("%c", name[j]);
-}
-printf("!\n");
+  //Print name using the char array
+  printf("Hello ");
+  for(j = 0; j < len-1; j++){
+    printf("%c", name[j]);
+  }
+  printf("!\n");
 
 }
